7d5b611481ea215aca3be5f3c35089022613b0e9
Asım K <sok1864@proton.me>
2023-01-21 16:13:00 +0300

    Ödevde belirtilen 2 fşle ve 1 grup file oluşturdum

---
+++ src/asim/Asim_1.java
@@ -0,0 +1,7 @@
+package asim;
+
+public class Asim_1 {
+    public static void main(String[] args) {
+        System.out.println("Asım 1 Deneme");
+    }
+}
---
+++ src/asim/Asim_2.java
@@ -0,0 +1,7 @@
+package asim;
+
+public class Asim_2 {
+    public static void main(String[] args) {
+        System.out.println("Asim 2 deneme");
+    }
+}
